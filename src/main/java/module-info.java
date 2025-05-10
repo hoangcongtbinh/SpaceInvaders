17204cf6bd341016de053bee94151a061044c079
Hoang Cong <hoangcongtbinh@gmail.com>
2025-05-11 00:09:25 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module uet.oop.space_invaders.spaceinvaders {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens uet.oop.space_invaders.spaceinvaders to javafx.fxml;
+    exports uet.oop.space_invaders.spaceinvaders;
+}
---
+++ src/main/java/uet/oop/space_invaders/spaceinvaders/HelloController.java
@@ -0,0 +1,14 @@
+package uet.oop.space_invaders.spaceinvaders;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
